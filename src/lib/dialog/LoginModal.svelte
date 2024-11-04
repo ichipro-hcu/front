ab6fbb84439888a77d4a826653fe212eafaea2eb
m-tsuru <147146974+m-tsuru@users.noreply.github.com>
2024-11-04 16:02:36 +0900

    Info: Pending

---
+++ src/lib/dialog/LoginModal.svelte
@@ -0,0 +1,9 @@
+<script>
+    import { Button, Modal } from 'flowbite-svelte';
+    let openLoginModalState = true
+</script>
+
+<Modal title="ログイン" bind:open={openLoginModalState} autoclose>
+    <p>ログインに同意した場合，<a href="https://sasakulab.com/termOfService.txt" class="text-blue-400">利用規約</a>に同意することになります．</p>
+    <Button class="text-center w-full">ログイン</Button>
+</Modal>
