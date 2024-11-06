d8ea36bcfc3d4894e40de2731fa3824903598799
m-tsuru <147146974+m-tsuru@users.noreply.github.com>
2024-11-07 02:28:19 +0900

    Fix: Displays the time in a locale-neutral format immediately after page load
    Co-authored-by: KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>

--- src/lib/widget/Clock.svelte
+++ src/lib/widget/Clock.svelte
@@ -2,11 +2,15 @@
     import { onMount } from 'svelte';
     import { Card } from 'flowbite-svelte';
 
-    let time = new Date();
+    let now = new Date();
+    let date = now.toLocaleDateString();
+    let time = now.toLocaleTimeString();
 
     onMount(() => {
         const interval = setInterval(() => {
-            time = new Date();
+            now = new Date();
+            date = now.toLocaleDateString();
+            time = now.toLocaleTimeString();
         }, 1000);
 
         return () => {
@@ -15,7 +19,7 @@
     });
 </script>
 
-<Card href="https://ichipol.g.hiroshima-cu.ac.jp/uprx/MobileShibbolethAuthServlet" class="w-12/12  max-w-none">
-    <span class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center" id="main-clock">{time.toLocaleTimeString()}</span>
-    <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight text-center" id="main-date">{time.toLocaleDateString()}</p>  
+<Card href="https://ichipol.g.hiroshima-cu.ac.jp/uprx/MobileShibbolethAuthServlet" class="w-12/12 max-w-none">
+    <span class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center" id="main-clock">{time}</span>
+    <p class="font-normal text-gray-700 dark:text-gray-400 leading-tight text-center" id="main-date">{date}</p>  
 </Card>
