e7c90e399b304442404be8140f70aa4bffecf766
m-tsuru <147146974+m-tsuru@users.noreply.github.com>
2025-04-10 11:28:57 +0900

    Fix: Bottom Nav Bar

--- src/lib/base/BottomNavigation.svelte
+++ src/lib/base/BottomNavigation.svelte
@@ -3,7 +3,7 @@
     import { HomeSolid, TableRowSolid, RectangleListSolid, UserCircleSolid } from 'flowbite-svelte-icons'
 </script>
 
-<BottomNav position="fixed" class="z-10" classInner="grid-cols-4" activeUrl="/">
+<BottomNav position="fixed" class="z-10" classInner="grid-cols-1" activeUrl="/">
     <BottomNavItem btnName="ブックマーク" href="/">
         <HomeSolid class="w-6 h-6 mb-1 text-gray-500 dark:text-gray-400 group-hover:text-primary-600 dark:group-hover:text-primary-500"/>
     </BottomNavItem>
